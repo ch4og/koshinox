8dfa319c41f113d5ac630d5cd164a9bdca6e8357
Nikita Mitasov <me@ch4og.com>
2025-11-28 04:17:48 +0300

    lib: remove extra code from config-root

--- shika/lib/config-root.scm
+++ shika/lib/config-root.scm
@@ -1,10 +1,7 @@
 ;;; SPDX-FileCopyrightText: 2025 Nikita Mitasov <me@ch4og.com>
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
-(define-module (shika lib config-root)
-  )
-(define-public config-root
-  (let* ((source-file (current-filename))
-         (abs-path (canonicalize-path source-file)))
-    (dirname abs-path)))
+(define-module (shika lib config-root))
 
+(define-public config-root
+  (dirname (canonicalize-path (current-filename))))
