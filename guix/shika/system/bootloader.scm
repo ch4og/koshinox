ad30c7a5fe7639c07c1d9a4e9209fffcbadfc6ed
Nikita Mitasov <me@ch4og.com>
2026-01-04 03:48:05 +0300

    system: bootloader: use argon2id capable grub fork

--- guix/shika/system/bootloader.scm
+++ guix/shika/system/bootloader.scm
@@ -1,4 +1,4 @@
-;;; SPDX-FileCopyrightText: 2025 Nikita Mitasov <me@ch4og.com>
+;;; SPDX-FileCopyrightText: 2025-2026 Nikita Mitasov <me@ch4og.com>
 ;;; SPDX-License-Identifier: GPL-3.0-or-later
 
 (define-module (shika system bootloader)
@@ -6,11 +6,12 @@
   #:use-module (gnu artwork)
   #:use-module (gnu bootloader)
   #:use-module (gnu bootloader grub)
+  #:use-module (rosenthal bootloader grub)
   #:use-module (shika lib layout))
 
 (define-public %shika-bootloader-configuration
   (bootloader-configuration
-   (bootloader grub-efi-bootloader)
+   (bootloader grub-efi-luks2-bootloader)
    (targets '("/efi"))
    (keyboard-layout %shika-layout)
    (theme (grub-theme
