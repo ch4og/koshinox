55e2e2c6692b4a53e32a959beafb3176506eb27d
Nikita Mitasov <me@ch4og.com>
2026-01-31 01:59:34 +0300

    system: services: update nix subs

--- koshi/system/services/config/nix.scm
+++ koshi/system/services/config/nix.scm
@@ -4,15 +4,21 @@
 (define-module (koshi system services config nix)
   #:use-module (gnu services nix))
 
+(define-public %koshi-nix-subs
+  '("https://mirror.yandex.ru/nixos"
+    "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+    "https://cache.nixos.kz"
+    "https://nixos-cache-proxy.cofob.dev"
+    "https://cache.nixos.org/"))
+
 (define-public %koshi-nix-configuration
   (nix-configuration
-   (extra-config
-    (list
-     (string-join
-      '("allowed-users = @wheel root"
-        "auto-optimise-store = true"
-        "experimental-features = nix-command flakes"
-        "substituters = https://nixos-cache-proxy.cofob.dev https://cache.nixos.org/"
-        "trusted-users = @wheel root"
-        "warn-dirty = false")
-      "\n")))))
+    (extra-config
+     (list
+      (string-join `("allowed-users = @wheel root"
+                     "auto-optimise-store = true"
+                     "experimental-features = nix-command flakes"
+                     ,(string-join `("substituters" "=" ,@%koshi-nix-subs) " ")
+                     "trusted-users = @wheel root"
+                     "warn-dirty = false")
+                   "\n")))))
