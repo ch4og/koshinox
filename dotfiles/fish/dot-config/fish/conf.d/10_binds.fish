7e71787b99bd3e2363834ea339bfd0030c1798a6
Nikita Mitasov <me@ch4og.com>
2026-02-19 12:29:45 +0300

    dotfiles: fish: add keybinds

---
+++ dotfiles/fish/dot-config/fish/conf.d/10_binds.fish
@@ -0,0 +1,7 @@
+bind ctrl-right forward-word
+bind ctrl-left backward-word
+bind alt-right nextd-or-forward-word
+bind alt-left prevd-or-backward-word
+bind alt-backspace backward-kill-word
+bind ctrl-alt-h backward-kill-word
+bind alt-delete kill-word
