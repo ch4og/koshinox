e29185959a4a5504305f8c289b33d5818f18d5db
Nikita Mitasov <me@ch4og.com>
2026-03-26 20:40:11 +0300

    home: packages: add opencode

--- koshi/home/packages/development.scm
+++ koshi/home/packages/development.scm
@@ -11,6 +11,7 @@
                                   "git-delta"
                                   "jujutsu"
                                   "lazygit"
+                                  "opencode"
                                   "passt"
                                   "podman"
                                   "podman-compose"
