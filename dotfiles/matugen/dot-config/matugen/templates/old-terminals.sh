6304b16e6ac0d4134bc35acb8df77b98352a0159
Nikita Mitasov <me@ch4og.com>
2026-03-26 19:49:15 +0300

    dotfiles: matugen: add opencode

--- dotfiles/matugen/dot-config/matugen/templates/old-terminals.sh
+++ dotfiles/matugen/dot-config/matugen/templates/old-terminals.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 COLOR_SCRIPT="$HOME/.config/term-colors.sh"
-
-buffer="$(sh "$COLOR_SCRIPT")"
+buffer="$(bash "$COLOR_SCRIPT")"
 
 succ=0
 
 for pts in /dev/pts/*; do
     [[ -w "$pts" ]] || continue
-    printf "%s" "$buffer" > "$pts" 2>/dev/null && ((succ++))
+
+    if timeout 0.1s bash -c 'exec 3>"$1"; printf "%s" "$2" >&3' _ "$pts" "$buffer" 2>/dev/null; then
+        ((succ++))
+    fi
 done
 
 if [[ -t 1 ]]; then
