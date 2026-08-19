ab8bc76b4619a5e946648853bd344bb71e4f9ae9
Nikita Mitasov <me@ch4og.com>
2026-08-19 17:20:39 +0300

    dotfiles: fish: add commented gpg fix file to use in case of gpg regression

---
+++ dotfiles/fish/dot-config/fish/conf.d/07_gpg.fish
@@ -0,0 +1,9 @@
+ # if status is-interactive
+ #     set -l tty_now (tty 2>/dev/null)
+
+ #     if test -n "$tty_now"
+ #         set -gx GPG_TTY "$tty_now"
+ #         set -gx SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+ #         gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
+ #     end
+ # end
