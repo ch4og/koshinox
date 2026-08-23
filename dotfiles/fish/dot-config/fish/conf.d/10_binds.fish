95fde292ed3a88f6c2b9e2f0fc2aabab8f16dedd
Nikita Mitasov <me@ch4og.com>
2026-08-24 00:20:09 +0300

    dotfiles: fish: fix backward-kill-word for vi keymode

--- dotfiles/fish/dot-config/fish/conf.d/10_binds.fish
+++ dotfiles/fish/dot-config/fish/conf.d/10_binds.fish
@@ -3,6 +3,7 @@ bind ctrl-right forward-word
 bind ctrl-left backward-word
 bind alt-right nextd-or-forward-word
 bind alt-left prevd-or-backward-word
-bind alt-backspace backward-kill-word
+bind -M insert \e\x7f backward-kill-word
+bind -M insert \e\[127\;2u backward-kill-word
 bind ctrl-alt-h backward-kill-word
 bind alt-delete kill-word
