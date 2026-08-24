1b382a1134b821f06c6021eee3cf424f43766521
Nikita Mitasov <me@ch4og.com>
2026-08-24 06:39:27 +0300

    dotfiles: fish: fix direnv loading in tmux

--- dotfiles/fish/dot-config/fish/conf.d/04_tools.fish
+++ dotfiles/fish/dot-config/fish/conf.d/04_tools.fish
@@ -2,5 +2,10 @@ if status is-interactive
     zoxide init fish | source
     fzf --fish | source
     starship init fish | source
+
+    if test -n "$TMUX"; and test -n "$DIRENV_DIR"
+        set --erase (set --names --export | string match --regex '^DIRENV_.*')
+    end
+
     direnv hook fish | source
 end
