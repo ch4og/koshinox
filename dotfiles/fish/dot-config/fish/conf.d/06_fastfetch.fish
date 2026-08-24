22cf6c0842612f6d6bc54eb03fa2773d608c7aa2
Nikita Mitasov <me@ch4og.com>
2026-08-24 06:39:53 +0300

    dotfiles: fish: update fastfetch

--- dotfiles/fish/dot-config/fish/conf.d/06_fastfetch.fish
+++ dotfiles/fish/dot-config/fish/conf.d/06_fastfetch.fish
@@ -1,9 +1,14 @@
 if status is-interactive
-    if test "$SHLVL" -eq 1; and not string match -q "linux" $TERM
-        if string match -q "*ghostty*" $TERM; or string match -q "*kitty*" $TERM
-            fastfetch --logo-type kitty -c ~/.config/fastfetch/autorun.jsonc
-        else
-            fastfetch -c ~/.config/fastfetch/autorun.jsonc
+    if test "$SHLVL" -eq 1; or begin
+        test -n "$TMUX"
+        and test "$SHLVL" -eq 2
+    end
+        if not string match -q "linux" $TERM
+            if string match -q "*ghostty*" $TERM; or string match -q "*kitty*" $TERM
+                fastfetch --logo-type kitty -c ~/.config/fastfetch/autorun.jsonc
+            else
+                fastfetch -c ~/.config/fastfetch/autorun.jsonc
+            end
         end
     end
 end
