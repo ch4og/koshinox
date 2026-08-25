1da0b7ac8b44875577d814bd041d7c14d1533b87
Nikita Mitasov <me@ch4og.com>
2026-08-25 14:24:43 +0300

    dotfiles: matugen: update terminal colors

--- dotfiles/matugen/dot-config/matugen/templates/terminal.sh
+++ dotfiles/matugen/dot-config/matugen/templates/terminal.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 
 colors=(
-    "{{colors.background.default.hex}}"
-    "{{colors.error_container.default.hex}}"
-    "{{colors.tertiary_container.default.hex}}"
-    "{{colors.outline.default.hex}}"
-    "{{colors.primary_container.default.hex}}"
-    "{{colors.secondary_container.default.hex}}"
-    "{{colors.primary.default.hex | lighten: 10.0}}"
-    "{{colors.on_surface.default.hex}}"
-    "{{colors.surface_variant.default.hex}}"
-    "{{colors.error.default.hex}}"
-    "{{colors.tertiary.default.hex}}"
-    "{{colors.on_surface_variant.default.hex}}"
-    "{{colors.primary.default.hex}}"
-    "{{colors.secondary.default.hex}}"
-    "{{colors.primary.default.hex | lighten: 20.0}}"
-    "{{colors.on_surface.default.hex | lighten: 5.0}}"
+    "{{colors.surface.dark.hex}}"
+    "{{colors.error.dark.hex | auto_lightness: 15}}"
+    "{{colors.tertiary.dark.hex | auto_lightness: 25}}"
+    "{{colors.tertiary.dark.hex | auto_lightness: 20}}"
+    "{{colors.secondary.dark.hex | auto_lightness: 25}}"
+    "{{colors.secondary.dark.hex | auto_lightness: 20}}"
+    "{{colors.primary.dark.hex | auto_lightness: 10}}"
+    "{{colors.on_surface_variant.dark.hex}}"
+    "{{colors.surface_container_high.dark.hex}}"
+    "{{colors.error.dark.hex | auto_lightness: 10}}"
+    "{{colors.tertiary.dark.hex | auto_lightness: 10}}"
+    "{{colors.tertiary_fixed.dark.hex | auto_lightness: 10}}"
+    "{{colors.secondary.dark.hex | auto_lightness: 10}}"
+    "{{colors.secondary_fixed.dark.hex | auto_lightness: 10}}"
+    "{{colors.primary.dark.hex | auto_lightness: -5}}"
+    "{{colors.on_surface.dark.hex}}"
 )
 
 for i in "${!colors[@]}"; do
     printf '\033]4;%d;%s\033\\' "$i" "${colors[$i]}"
 done
 
-printf '\033]11;{{colors.background.default.hex}}\033\\'
-printf '\033]10;{{colors.on_surface.default.hex}}\033\\'
-printf '\033]12;{{colors.primary.default.hex}}\033\\'
-
-printf '\033]7044;{{colors.primary.dark.hex}}\033\\'
+printf '\033]11;{{colors.surface.dark.hex}}\033\\'
+printf '\033]19;{{colors.surface.dark.hex}}\033\\'
+printf '\033]4;232;{{colors.surface.dark.hex}}\033\\'
+printf '\033]4;257;{{colors.surface.dark.hex}}\033\\'
+printf '\033]708;{{colors.surface.dark.hex}}\033\\'
+printf '\033]10;{{colors.on_surface.dark.hex}}\033\\'
+printf '\033]17;{{colors.on_surface.dark.hex}}\033\\'
+printf '\033]4;256;{{colors.on_surface.dark.hex}}\033\\'
+printf '\033]12;{{colors.on_surface.dark.hex}}\033\\'
+printf '\033]13;{{colors.on_surface.dark.hex}}\033\\'
