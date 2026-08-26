d4b1f99a5153df53c397752e4701aef88c40f104
Nikita Mitasov <me@ch4og.com>
2026-08-26 14:36:05 +0300

    dotfiles: matugen: add firefox-websites

---
+++ dotfiles/matugen/dot-config/matugen/scripts/websites.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.config/librewolf/librewolf/0zrpirfb.default/chrome/websites
+cp ~/.config/matugen/userContent.css ~/.config/librewolf/librewolf/0zrpirfb.default/chrome/
+cp ~/.config/matugen/websites/* ~/.config/librewolf/librewolf/0zrpirfb.default/chrome/websites/
